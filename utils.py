92497cbb65d41b71a02d8976d5b2f372d1709038
Dragos Ungureanu <dragosungureanu90@gmail.com>
2023-11-02 09:06:11 +0200

    Update utils.py
    Added the for loops to find the best number for the n estimators. This was the final part of the best_forest_regressor function.

--- utils.py
+++ utils.py
@@ -91,3 +91,44 @@ def best_forest_regressor(x_train, y_train, x_cv, y_cv):
     plt.plot(mse_list_train)
     plt.plot(mse_list_cv)
     plt.legend(['Train', 'Validation'])
+
+    #Third loop to find the best n estimators
+    mse_list_train = []
+    mse_list_cv = []
+    all_mse_train = np.zeros(0)
+    all_mse_cv = np.zeros(0)
+    diff_mse = np.zeros(0)
+    best_n_estimators = 0
+
+    for n_estimators in n_estimators_list:
+        # You can fit the model at the same time you define it, because the fit function returns the fitted estimator.
+        model = RandomForestRegressor(n_estimators=n_estimators,
+                                      random_state=1234).fit(x_train, y_train)
+        predictions_train = model.predict(x_train)  ## The predicted values for the train dataset
+        predictions_cv = model.predict(x_cv)  ## The predicted values for the test dataset
+        mse_train = mean_squared_error(predictions_train, y_train)
+        mse_cv = mean_squared_error(predictions_cv, y_cv)
+        mse_list_train.append(mse_train)
+        mse_list_cv.append(mse_cv)
+        all_mse_train = np.append(all_mse_train, mse_train)
+        all_mse_cv = np.append(all_mse_cv, mse_cv)
+        diff_mse = np.append(diff_mse, (mse_cv - mse_train))
+    for i, (v, j, k) in enumerate(zip(diff_mse, all_mse_train, all_mse_cv)):
+        if v < np.mean(diff_mse) and v > 0:
+            if j < np.mean(all_mse_train) and k < np.mean(all_mse_cv):
+                best_n_estimators = n_estimators_list[i]
+                break
+    plt.subplot(1, 3, 3)
+    plt.title('Train x Validation metrics')
+    plt.xlabel('max_depth')
+    plt.ylabel('mse')
+    plt.xticks(ticks=range(len(n_estimators_list)), labels=n_estimators_list)
+    plt.plot(mse_list_train)
+    plt.plot(mse_list_cv)
+    plt.legend(['Train', 'Validation'])
+
+    plt.tight_layout()
+    print(f"The selected best min splits is {best_min_split}, best max depth is {best_max_deph} "
+          f"and the best n estimators is {best_n_estimators}.")
+
+    return best_min_split, best_max_deph, best_n_estimators
