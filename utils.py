aa547d2ffcdcbc20e1a3854aef8d1dc61ab7046c
Dragos Ungureanu <dragosungureanu90@gmail.com>
2023-11-02 09:06:54 +0200

    Update utils.py
    added first part of a new function called best_forest_regressor.

--- utils.py
+++ utils.py
@@ -132,3 +132,18 @@ def best_forest_regressor(x_train, y_train, x_cv, y_cv):
           f"and the best n estimators is {best_n_estimators}.")
 
     return best_min_split, best_max_deph, best_n_estimators
+
+def best_forest_regressor(x_train, y_train, x_cv, y_cv):
+    """
+    Computes the best number based on the provided data for the min samples split, max depth and n estimators.
+    These three will be further used for finding the best features based on forest regressor.
+    Finally, a graph will be produced to show the mean squared error for each parameter.
+
+    :param x_train: the train data sample of all the numeric independent features
+    :param y_train: the target data sample for the train sample in a numeric format
+    :param x_cv: the cross-validation sample of all the numeric independent features
+    :param y_cv: the target data sample for the cross validation sample in a numeric format
+    :return: best_min_split - best min sample split number to be used in the forest regressor parameters
+             best_max_deph - best max depth number to be used in the forest regressor parameters
+             best_n_estimators - best n estimators number to be used in the forest regressor parameters
+    """
