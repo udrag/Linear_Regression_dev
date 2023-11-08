de599d8a12c9bc88d8314b13de5d80517164f776
Dragos Ungureanu <dragosungureanu90@gmail.com>
2023-11-08 10:13:07 +0200

    Update utils.py
    general update of all functions

--- utils.py
+++ utils.py
@@ -1,12 +1,16 @@
 import numpy as np
 import pandas as pd
 import math
+import copy
+
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 import tensorflow as tf
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, LeakyReLU
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
 class BestParam:
 
@@ -77,6 +81,8 @@ class BestParam:
         print(f"The selected best min splits is {best_param_values[0]}, best max depth is {best_param_values[1]} and the best n estimators is {best_param_values[2]}.")
         return best_param_values[0], best_param_values[1], best_param_values[2]
 
+
+
 def feature_importance(x_train, y_train, min_split, max_depth, n_estimators):
     """
     Computes the gini score for each feature using a Random Forest Regressor with the specified parameters.
@@ -135,7 +141,7 @@ def plot_features(x, y):
 
     return plt.show()
 
-def linear_regeression_feature_performance(x_train, y_train, x_cv, y_cv):
+def linear_regeression_feature_performance(x_train, y_train, x_cv, y_cv, selected_features):
     """
     Computes the mean squared error for adding a feature one by one starting from the first one. Additionally,
     will transform the values of the feature through 4 polynomial degrees.
@@ -144,43 +150,43 @@ def linear_regeression_feature_performance(x_train, y_train, x_cv, y_cv):
     :param y_train: the target values of training set
     :param x_cv: the independent variables values of the cross validation set
     :param y_cv: the target values of cross validation set
-    :return:
+    :return: plt.show()
     """
+    index_selected_features = len(selected_features) - 1
     fig, axes = plt.subplots(2, 2, figsize=(10, 10), constrained_layout=True)
-    axes = axes.flatten()
+    axes      = axes.flatten()
 
     for d in range(1, 5):
         mse_train_list = []
-        mse_cv_list = []
+        mse_cv_list    = []
 
         for i in range(1, len(x_train.columns) + 1):
-            standard = StandardScaler()
-            train_std = standard.fit_transform(x_train.iloc[:, 0:i])
+            standard       = StandardScaler()
+            train_std      = standard.fit_transform(x_train.iloc[:, 0:i])
 
-            polyn = PolynomialFeatures(degree=d, include_bias=False)
+            polyn          = PolynomialFeatures(degree=d, include_bias=False)
             train_std_poly = polyn.fit_transform(train_std)
 
-            model = LinearRegression()
+            model          = LinearRegression()
             model.fit(train_std_poly, y_train)
 
-            pred_train = model.predict(train_std_poly)
-            mse_train = mean_squared_error(y_train, pred_train)
+            pred_train     = model.predict(train_std_poly)
+            mse_train      = mean_squared_error(y_train, pred_train)
             mse_train_list.append(mse_train)
 
-            x_cv_std = standard.transform(x_cv.iloc[:, 0:i])
+            x_cv_std      = standard.transform(x_cv.iloc[:, 0:i])
             x_cv_std_poly = polyn.transform(x_cv_std)
-            pred_cv = model.predict(x_cv_std_poly)
-            mse_cv = mean_squared_error(y_cv, pred_cv)
+            pred_cv       = model.predict(x_cv_std_poly)
+            mse_cv        = mean_squared_error(y_cv, pred_cv)
             mse_cv_list.append(mse_cv)
-        print(f"The MSE of the {d} polynial degree for the cross validation set is {mse_cv}")
-        
+
         x_positions = np.arange(len(x_train.columns))
-        x_labels = x_train.columns
+        x_labels    = x_train.columns
 
-        min_value = min(mse_train_list + mse_cv_list) * 0.95
-        max_value = min(mse_cv_list) * 2
+        min_value   = min(mse_train_list + mse_cv_list) * 0.95
+        max_value   = min(mse_cv_list) * 2
 
-        axes[d - 1].set_title(f'Train x Validation x Test for {d} degree')
+        axes[d - 1].set_title(f'Train vs Validation for {d} degree')
         axes[d - 1].plot(x_positions, mse_train_list, label='Train')
         axes[d - 1].plot(x_positions, mse_cv_list, label='Validation')
         axes[d - 1].set_ylim(min_value, max_value)
@@ -188,6 +194,7 @@ def linear_regeression_feature_performance(x_train, y_train, x_cv, y_cv):
         axes[d - 1].set_xticklabels(x_labels, rotation=85)
         axes[d - 1].set_xlabel('Features added')
         axes[d - 1].set_ylabel('MSE')
+        axes[d - 1].vlines(index_selected_features, 0, max_value, color='r', linestyle='--', label='Selected Features')
         axes[d - 1].legend()
 
     # Hide any remaining empty subplots (if any)
@@ -196,121 +203,99 @@ def linear_regeression_feature_performance(x_train, y_train, x_cv, y_cv):
 
     return plt.show()
 
-def plot_actual_vs_predicted(y_test, *args, observations=30):
-    """
-    Plot the values of the actual y and the predicted ones. The first argument always should be the actual values of y.
-    :param *args: place any number of dataframe/numpay arrays to be plotted
-    :param observations: integer representing the number of last observations to plot.
-                         Default value of 30 last observations.
-    :return: plt.show()
-    """
-    observations = np.negative(observations+1)
-    check_test = pd.DataFrame(y_test)
-    for i, arg in enumerate(args):
-        check_test = pd.concat(
-            [check_test.reset_index(drop=True),
-             pd.DataFrame(arg).reset_index(drop=True)
-             ], axis=1)
-    plt.figure(figsize=(15,8))
-    plt.plot(check_test.iloc[observations:-1, 0], label='Actual', marker='o')
-    plt.xlabel('Prediction number')
-    plt.ylabel('MSE')
-    for i, _ in enumerate(args):
-        plt.xticks()
-        plt.plot(check_test.iloc[observations:-1,i+1], label=f'Predicted {i+1}', marker='o')
-        plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
-        plt.legend()
-        plt.tight_layout()
-    return plt.show()
 
-def gradient_descent(x_train, y_train, x_cv, y_cv, x_test, y_test, alpha, num_iters, poly_degree=1):
+def linear_neural_regression(x_train, y_train, x_cv, y_cv, x_test, y_test, max_degree=4, epochs=300, verbose=0, learning_rate=0.001):
     """
-    Performs linear regression with gradient descent to learn w and b. Updates w and b by taking
-    num_iters gradient steps with learning rate alpha.
+    Neural network with 4 Dense layers (64, 32, 15, 1) and each with a linear activator. The function runs several models in accordance
+    with the indicated degree of polynomial. The model is selected by the smallest Mean Squared Error assessed via the sklearn library.
+    A graph will show the mean squared error for each degree of polynomial.
+    Finally, the function will also calculate the Mean Squared Error for the test set based on the performance of the selected model.
 
-    :param x_train: Train sample
-    :param y_train: Train target values
-    :param x_cv: Cross Validation sample
-    :param y_cv: Cross Validation target values
+    :param x_train: the train data sample of all the numeric independent features
+    :param y_train: the target data sample for the train sample in a numeric format
+    :param x_cv: the cross-validation sample of all the numeric independent features
+    :param y_cv: the target data sample for the cross validation sample in a numeric format
     :param x_test: the test sample of all the numeric independent features
     :param y_test: the target data sample for the test sample in a numeric format
-    :param alpha: Learning rate
-    :param num_iters: number of iterations to run gradient descent
-    :param poly_degree: The degree of polynomial to transform the variables
-    :return: w - final weight coefficient
-             b - final bias coefficient
-             pred_test - predictions of the test sample
-             J_history - the cost value at each itteration
+    :param max_degree: the maximal polynomial degree to transform the variables
+    :param epochs: an integer that constitutes the number of epochs each model will be run on, default 300
+    :param verbose: an integer that suppresses the text notation of each step in the modelling phase
+    :return: selected_model - the selected model data
+             standardscale  - scaling data to transform features
+             polynomialft   - polynomial data to transform features
+             pred_test      - predictions of the test sample
+             mse_test       - the mean squared error on the test sample
     """
+    cv_errors    = np.zeros(0)
+    train_errors = np.zeros(0)
+    all_models   = {}
+    all_standard = {}
+    all_poly     = {}
+    degrees      = {}
+    for i in range(1, max_degree + 1):
+        standard             = StandardScaler()
+        train_std            = standard.fit_transform(x_train)
 
-    # Fit-transform train data
-    standard = StandardScaler()
-    train_std = standard.fit_transform(x_train)
-    polyn = PolynomialFeatures(degree=poly_degree, include_bias=False)
-    train_std_poly = polyn.fit_transform(train_std)
-    # Transform cross validation data
-    cv_std = standard.transform(x_cv)
-    cv_std_poly = polyn.transform(cv_std)
+        poly                 = PolynomialFeatures(degree=i, include_bias=False)
+        train_std_poly       = poly.fit_transform(train_std)
+
+        model = tf.keras.models.Sequential()
+        model.add(Dense(units=24, activation=LeakyReLU(alpha=0.2)))
+        model.add(Dense(units=16, activation=LeakyReLU(alpha=0.2)))
+        model.add(Dense(units=8, activation=LeakyReLU(alpha=0.2)))
+        model.add(Dense(units=1, activation='linear'))
+        model.compile(
+            loss=tf.keras.losses.mean_squared_error,
+            optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=learning_rate),
+        )
+
+        model.fit(train_std_poly, y_train, epochs=epochs, verbose=verbose)
+
+        pred_train           = model.predict(train_std_poly)
+        mse_train            = mean_squared_error(y_train, pred_train)
+        train_errors         = np.append(train_errors, mse_train)
+
+        x_cv_std             = standard.transform(x_cv)
+        x_cv_std_poly        = poly.transform(x_cv_std)
+        pred_cv              = model.predict(x_cv_std_poly)
+        mse_cv               =  mean_squared_error(y_cv, pred_cv)
+        cv_errors            = np.append(cv_errors, mse_cv)
+        # Append the model data
+        all_models[str(i)]   = model
+        all_standard[str(i)] = standard
+        all_poly[str(i)]     = poly
+        degrees[str(i)]      = i
+        print(f"Development of the model with polynomial degree of {i}\nThe MSE for the train set is {mse_train} and the Cross Validation is: {mse_cv}")
+
+    diff_mse = cv_errors - train_errors
+    for i, (v, j, k) in enumerate(zip(diff_mse, train_errors, cv_errors)):
+        if v <= np.min(diff_mse)*1.2 and v > 0 and k < np.min(cv_errors) * 1.2:
+            index_best_model = i + 1
+
+    x_values = range(1, max_degree+1)
+
+    plt.figure()
+    plt.title('Train x Validation metrics')
+    plt.xlabel('Polynomial degrees')
+    plt.ylabel('MSE')
+    plt.xticks(ticks=x_values, labels=x_values)
+    plt.plot(x_values, train_errors)
+    plt.plot(x_values, cv_errors)
+    plt.legend(['Train', 'Validation'])
+    plt.tight_layout()
+
+    # Assign the best model
+    selected_model    = all_models[str(index_best_model)]
+    standard_selected = all_standard[str(index_best_model)]
+    ploy_selected     = all_poly[str(index_best_model)]
     # Transform test data
-    test_std = standard.transform(x_test)
-    test_std_poly = polyn.transform(test_std)
-    # An array to store cost J and w's at each iteration primarily for observing cost reduction during development
-    J_history = []
+    x_test_std        = standard_selected.transform(x_test)
+    x_test_std_poly   = ploy_selected.transform(x_test_std)
+    pred_test         = selected_model.predict(x_test_std_poly)
+    mse_test          = mean_squared_error(y_test, pred_test)
 
-    y_train = y_train.to_numpy().reshape((-1,1)) # Reshape the target variable for the train sample
-    y_cv = y_cv.to_numpy().reshape((-1,1)) # Reshape the target variable for the cross validation sample
-    y_test = y_test.to_numpy().reshape((-1,1)) # Reshape the target variable for the test sample
-    m, n = train_std_poly.shape   #(number of examples, number of features)
-    w = np.zeros(n)
-    b = 0.
-    dj_dw = np.zeros((n,))
-    dj_db = 0.
-    cost = 0.0
-
-    for i in range(num_iters):
-        # Calculate the gradient and update the parameters
-        for j in range(m):
-            err = (np.dot(train_std_poly[j], w) + b) - y_train[j]
-            for f in range(n):
-                dj_dw[f] = dj_dw[f] + err * train_std_poly[j, f]
-            dj_db = dj_db + err
-        dj_dw = dj_dw / m
-        dj_db = dj_db / m
-
-        # Update Parameters using w, b, alpha and gradient
-        w = w - alpha * dj_dw
-        b = b - alpha * dj_db
-        # Stop gradient decent when the 4 decimals rounded of the last 10 cost function values equal to the current cost value
-        if len(J_history) > 100:
-            if (np.round(J_history[-10:-1],4) == np.round(cost, 4)).all() == True:
-                break
-            else:
-                # Print cost at specified intervals
-                if i% math.ceil(num_iters / 10) == 0:
-                    print(f"Iteration {i}: Cost {J_history[-1]}")
-        # Save cost J at each iteration
-        for c in range(m):
-            f_wb_i = np.dot(train_std_poly[c], w) + b
-            cost = cost + (f_wb_i - y_train[c])**2
-        cost = cost / (2 * m)
-        J_history.append(cost)
-
-    # Predict the train values based on final w and b values
-    pred_train = np.dot(train_std_poly, w) + b
-    # Calculate the mean squared error for the train sample
-    mse_train = mean_squared_error(y_train, pred_train)
-    # Predict the cross validation values based on final w and b values
-    pred_cv = np.dot(cv_std_poly, w) + b
-    # Calculate the mean squared error for the cross validation sample
-    mse_cv = mean_squared_error(y_cv, pred_cv)
-    # Predict the test values based on final w and b values
-    pred_test = np.dot(test_std_poly, w) + b
-    # Calculate the mean squared error for the test sample
-    mse_test = mean_squared_error(y_test, pred_test)
-    print(f"The mean squared error for the train sample is: {mse_train}, the mean squared error for the cross "
-          f"validation sample is: {mse_cv} and for the test set is: {mse_test}")
-
-    return w, b, pred_test, J_history
+    print(f"The mean squared error of the selected model of {index_best_model} polynomial degree on the test sample is {mse_test}")
+    return selected_model, standard_selected, ploy_selected, pred_test, mse_test
 
 def linear_regression_ols(x_train, y_train, x_cv, y_cv, x_test, y_test, max_degree=5):
     """
@@ -395,10 +380,9 @@ def linear_regression_ols(x_train, y_train, x_cv, y_cv, x_test, y_test, max_degr
     pred_test         = selected_model.predict(test_std_poly)
     # Calculate the mean squared error for the test sample
     mse_test          = mean_squared_error(y_test, pred_test)
-    print(f"The mean squared error of the selected model of {index_best_model + 1} polynomial degree on the test sample is {mse_test}")
+    print(f"The mean squared error of the selected model of {index_best_model} polynomial degree on the test sample is {mse_test}")
 
     return selected_model, standard_selected, ploy_selected, pred_test, mse_test
-
 
 def linear_regression_gradient_descent(x_train, y_train, x_cv, y_cv, x_test, y_test, alpha, num_iters, poly_degree=1):
     """
@@ -437,7 +421,6 @@ def linear_regression_gradient_descent(x_train, y_train, x_cv, y_cv, x_test, y_t
     dj_dw     = np.zeros((n,))
     dj_db     = 0.
     cost      = 0.0
-
 
     for i in range(num_iters):
         # Calculate the gradient and update the parameters
@@ -491,3 +474,52 @@ def linear_regression_gradient_descent(x_train, y_train, x_cv, y_cv, x_test, y_t
           f"validation sample is: {mse_cv} and for the test set is: {mse_test}")
 
     return w, b, standard_gd, poly_gd, pred_test, mse_test
+
+class predict_gd:
+    def __init__(self, w, b):
+        self.w = w
+        self.b = b
+    def predict(self, x):
+        predictions = np.dot(x, self.w) + self.b
+        return predictions
+
+def best_regression(all_mse, all_models, all_standardscaler, all_polyft):
+    min_mse = min(all_mse, key=all_mse.get)
+
+    if min_mse != 'gradient':
+        selected_model = all_models[min_mse]
+        standardization = all_standardscaler[min_mse]
+        polynomial_transformation = all_polyft[min_mse]
+    if min_mse == 'gradient':
+        selected_model = predict_gd(w=all_models['gradient']['w'], b=all_models['gradient']['b'])
+        standardization = all_standardscaler[min_mse]
+        polynomial_transformation = all_polyft[min_mse]
+    return selected_model, standardization, polynomial_transformation
+
+def plot_actual_vs_predicted(y_test, *args, observations=30):
+    """
+    Plot the values of the actual y and the predicted ones. The first argument always should be the actual values of y.
+    :param *args: place any number of dataframe/numpay arrays to be plotted
+    :param observations: integer representing the number of last observations to plot.
+                         Default value of 30 last observations.
+    :return: plt.show()
+    """
+    observations = np.negative(observations+1)
+    check_test   = pd.DataFrame(y_test)
+    for i, arg in enumerate(args):
+        check_test = pd.concat(
+            [check_test.reset_index(drop=True),
+             pd.DataFrame(arg).reset_index(drop=True)
+             ], axis=1)
+    plt.figure(figsize=(15,6))
+    plt.plot(check_test.iloc[observations:-1, 0], label='Actual', marker='o')
+    plt.xlabel('Prediction number')
+    plt.ylabel('MSE')
+    for i, _ in enumerate(args):
+        plt.xticks()
+        plt.plot(check_test.iloc[observations:-1,i+1], label=f'Predicted {i+1}', marker='o')
+        plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
+        plt.legend()
+        plt.tight_layout()
+    return plt.show()
+# %%
